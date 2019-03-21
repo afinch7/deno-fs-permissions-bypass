6c05b8edfd1be5ddae1b8d0680a3f9550183431f
andy finch <andyfinch7@gmail.com>
2019-03-21 12:53:37 -0400

    update dispatch.ts for v0.3.4

--- dispatch.ts
+++ dispatch.ts
@@ -8,31 +8,41 @@ import * as util from "./util.ts";
 let nextCmdId = 0;
 const promiseTable = new Map<number, util.Resolvable<msg.Base>>();
 
-let fireTimers: () => void;
-
-export function setFireTimersCallback(fn: () => void) {
-  fireTimers = fn;
+export function handleAsyncMsgFromRust(ui8: Uint8Array): void {
+  const bb = new flatbuffers.ByteBuffer(ui8);
+  const base = msg.Base.getRootAsBase(bb);
+  const cmdId = base.cmdId();
+  const promise = promiseTable.get(cmdId);
+  util.assert(promise != null, `Expecting promise in table. ${cmdId}`);
+  promiseTable.delete(cmdId);
+  const err = errors.maybeError(base);
+  if (err != null) {
+    promise!.reject(err);
+  } else {
+    promise!.resolve(base);
+  }
 }
 
-export function handleAsyncMsgFromRust(ui8: Uint8Array) {
-  // If a the buffer is empty, recv() on the native side timed out and we
-  // did not receive a message.
-  if (ui8 && ui8.length) {
-    const bb = new flatbuffers.ByteBuffer(ui8);
-    const base = msg.Base.getRootAsBase(bb);
-    const cmdId = base.cmdId();
-    const promise = promiseTable.get(cmdId);
-    util.assert(promise != null, `Expecting promise in table. ${cmdId}`);
-    promiseTable.delete(cmdId);
-    const err = errors.maybeError(base);
-    if (err != null) {
-      promise!.reject(err);
-    } else {
-      promise!.resolve(base);
-    }
-  }
-  // Fire timers that have become runnable.
-  fireTimers();
+function sendInternal(
+  builder: flatbuffers.Builder,
+  innerType: msg.Any,
+  inner: flatbuffers.Offset,
+  zeroCopy: undefined | ArrayBufferView,
+  sync = true
+): [number, null | Uint8Array] {
+  const cmdId = nextCmdId++;
+  msg.Base.startBase(builder);
+  msg.Base.addInner(builder, inner);
+  msg.Base.addInnerType(builder, innerType);
+  msg.Base.addSync(builder, sync);
+  msg.Base.addCmdId(builder, cmdId);
+  builder.finish(msg.Base.endBase(builder));
+
+  const control = builder.asUint8Array();
+  const response = window.DenoCore.dispatch(control, zeroCopy);
+
+  builder.inUse = false;
+  return [cmdId, response];
 }
 
 // @internal
@@ -42,8 +52,14 @@ export function sendAsync(
   inner: flatbuffers.Offset,
   data?: ArrayBufferView
 ): Promise<msg.Base> {
-  const [cmdId, resBuf] = sendInternal(builder, innerType, inner, data, false);
-  util.assert(resBuf == null);
+  const [cmdId, response] = sendInternal(
+    builder,
+    innerType,
+    inner,
+    data,
+    false
+  );
+  util.assert(response == null);
   const promise = util.createResolvable<msg.Base>();
   promiseTable.set(cmdId, promise);
   return promise;
@@ -56,34 +72,14 @@ export function sendSync(
   inner: flatbuffers.Offset,
   data?: ArrayBufferView
 ): null | msg.Base {
-  const [cmdId, resBuf] = sendInternal(builder, innerType, inner, data, true);
+  const [cmdId, response] = sendInternal(builder, innerType, inner, data, true);
   util.assert(cmdId >= 0);
-  if (resBuf == null) {
+  if (response == null || response.length === 0) {
     return null;
   } else {
-    const u8 = new Uint8Array(resBuf!);
-    const bb = new flatbuffers.ByteBuffer(u8);
+    const bb = new flatbuffers.ByteBuffer(response);
     const baseRes = msg.Base.getRootAsBase(bb);
     errors.maybeThrowError(baseRes);
     return baseRes;
   }
-}
-
-function sendInternal(
-  builder: flatbuffers.Builder,
-  innerType: msg.Any,
-  inner: flatbuffers.Offset,
-  data: undefined | ArrayBufferView,
-  sync = true
-): [number, null | Uint8Array] {
-  const cmdId = nextCmdId++;
-  msg.Base.startBase(builder);
-  msg.Base.addInner(builder, inner);
-  msg.Base.addInnerType(builder, innerType);
-  msg.Base.addSync(builder, sync);
-  msg.Base.addCmdId(builder, cmdId);
-  builder.finish(msg.Base.endBase(builder));
-  const res = libdeno.send(builder.asUint8Array(), data);
-  builder.inUse = false;
-  return [cmdId, res];
 }
